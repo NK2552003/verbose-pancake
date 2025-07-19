2184ac9f09373e0dad0c73937f988116c451b66c
Nitish <nk2552003@gmail.com>
2025-07-19 07:16:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,56 @@
 import type { NextConfig } from "next";
+import { Headers } from "next/dist/server/config";
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: https: blob:;
+  connect-src 'self' https://api.github.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+`;
+
+const securityHeaders: Headers = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, "").trim(),
+  },
+  {
+    key: "Referrer-Policy",
+    value: "strict-origin-when-cross-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+];
 
 const nextConfig: NextConfig = {
-  devIndicators: false,
-  /* config options here */
+  devIndicators: {
+    buildActivity: false,
+  },
   images: {
-    domains: ["picsum.photos"], // Add the required domain
+    domains: ["picsum.photos"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
